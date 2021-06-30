029c79e1cc04caf808e3fe8a0c96206d02382b5e
Tilzen <lucas.nascimentoR@hotmail.com>
2021-06-30 01:03:52 -0300

    feat: Adiciona novos record types ao enum QueryType.

--- src/protocol/query_type.rs
+++ src/protocol/query_type.rs
@@ -1,7 +1,11 @@
 #[derive(PartialEq, Eq, Debug, Clone, Hash, Copy)]
 pub enum QueryType {
     UNKNOWN(u16),
-    A, // A = 1
+    A,     // A = 1
+    NS,    // NS = 2
+    CNAME, // CNAME = 5
+    MX,    // MX = 15
+    AAAA,  // AAAA = 28
 }
 
 impl QueryType {
